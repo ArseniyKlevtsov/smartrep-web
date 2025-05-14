0eeffb1e1033d30bf4069757bb8d17fbf829ad88
Arseniy <arseniyklevts@gmail.com>
2025-05-14 23:14:58 +0300

    add empty components

---
+++ src/app/small-components/profile-shortcut/profile-shortcut.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-shortcut',
+  imports: [],
+  templateUrl: './profile-shortcut.component.html',
+  styleUrl: './profile-shortcut.component.css'
+})
+export class ProfileShortcutComponent {
+
+}
