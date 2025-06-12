234ba946757d55b44f1885c59cfc19e8bdb2b39c
Arseniy <arseniyklevts@gmail.com>
2025-06-12 04:20:01 +0300

    fix  footer and profile-page

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   imports: [],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrl: './footer.component.css',
 })
 export class FooterComponent {
   currentYear: number = new Date().getFullYear();
+
+  constructor(private router: Router) {}
+
+  navigateToCourses(): void {
+    this.router.navigate(['/all-courses']);
+  }
+
+  navigateToProfile(): void {
+    this.router.navigate(['/profile']);
+  }
+
+  navigateToTeachers(): void {
+    this.router.navigate(['/all-teachers']);
+  }
 }
